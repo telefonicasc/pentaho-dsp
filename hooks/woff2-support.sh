c9f4554230846871f94735bd026994782124299b
rg2011 <rg2011>
2021-08-11 19:12:54 +0200

    Fixed support to download woff2 fonts

---
+++ hooks/woff2-support.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2020 Telefónica Soluciones de Informática y Comunicaciones de España, S.A.U.
+#
+# This file is part of Pentaho DSP.
+#
+# Pentaho DSP is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Pentaho DSP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+#
+# For those usages not covered by this license please contact with
+# sc_support at telefonica dot com
+
+set -eoux pipefail
+
+export CONFFILE="${PENTAHO_HOME}/pentaho-solutions/system/pentaho-cdf/settings.xml"
+if grep downloadable-formats "${CONFFILE}" | grep woff2; then
+  exit 0
+fi
+
+echo "ACTIVANDO soporte de descarga de fuentes WOFF2 en ${CONFFILE}"
+# Agrego la linea delante de la definicion del MimeType pdf, por ejemplo.
+sed -i "s/<downloadable-formats>/<downloadable-formats>woff2,/" "${CONFFILE}"
